8587a2aac82d09640ce1e47c93c29229334d152a
Aviwasser <33319477+Aviwasser@users.noreply.github.com>
2018-05-23 22:50:04 +0300

    Create DummyPlayers.cpp

---
+++ DummyPlayers.cpp
@@ -0,0 +1,14 @@
+#include "DummyPlayers.h"
+
+
+const Coordinate XYPlayer::play(const Board& board) {
+	for (uint x=0; x<board.size(); ++x) {
+		for (uint y=0; y<board.size(); ++y) {
+			Coordinate c{x,y};
+			if (board[c]=='.') {
+				return c;
+			}
+		}
+	}
+	return {0,0};  // did not find an empty square - play on the top-left
+}
